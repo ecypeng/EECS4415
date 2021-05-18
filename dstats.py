7ee918230cb0a3619b00e99a22c7ced1c6c827b2
Eric <31872664+ecypeng@users.noreply.github.com>
2021-05-18 09:17:55 -0400

    Add files via upload

--- dstats.py
+++ dstats.py
@@ -11,3 +11,21 @@ data = pandas.read_csv(targetFile)
 
 numOfBus = data.loc[data['city'] == cityName].city.count()
 print(numOfBus)
+
+avgStars = data.loc[data['city'] == cityName].stars.mean()
+print(avgStars)
+
+restaurantCategory = data.loc[data['categories'].str.contains('Restaurants')]
+numOfRestaurants = restaurantCategory.loc[data['city'] == cityName].city.count()
+print(numOfRestaurants)
+
+restaurantCategory = data.loc[data['categories'].str.contains('Restaurants')]
+avgStarsRestaurants = restaurantCategory.loc[data['city'] == cityName].stars.mean()
+print(avgStarsRestaurants)
+
+avgNumOfReviews = data.loc[data['city'] == cityName].review_count.mean()
+print(avgNumOfReviews)
+
+restaurantCategory = data.loc[data['categories'].str.contains('Restaurants')]
+avgNumOfReviewsBus = restaurantCategory.loc[data['city'] == cityName].review_count.mean()
+print(avgNumOfReviewsBus)
