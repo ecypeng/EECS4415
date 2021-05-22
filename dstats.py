84c158cfb804f59e6eda97db4899fe850d920600
CelinaAssal <45611903+CelinaAssal@users.noreply.github.com>
2021-05-22 17:08:33 -0400

    Update dstats.py

--- dstats.py
+++ dstats.py
@@ -9,23 +9,32 @@ cityName = sys.argv[2]
 # get the target CSV file
 data = pandas.read_csv(targetFile)
 
+# getting the city that was entered in the command line and getting the count
 numOfBus = data.loc[data['city'] == cityName].city.count()
 print(numOfBus)
 
+# getting the city that was entered in the command line and getting the mean of their stars
 avgStars = data.loc[data['city'] == cityName].stars.mean()
 print(avgStars)
 
+# getting the categories that contain Restaurants
 restaurantCategory = data.loc[data['categories'].str.contains('Restaurants')]
+# getting the restuarants in the city that was enterd in the command line and getting its count
 numOfRestaurants = restaurantCategory.loc[data['city'] == cityName].city.count()
 print(numOfRestaurants)
 
+# getting the categories that contain Restuarants
 restaurantCategory = data.loc[data['categories'].str.contains('Restaurants')]
+# calculating the average stars by getting the city that was entered in the command line and getting the mean of their stars
 avgStarsRestaurants = restaurantCategory.loc[data['city'] == cityName].stars.mean()
 print(avgStarsRestaurants)
 
+# getting the city entered in the command line and getting the average of the review_count
 avgNumOfReviews = data.loc[data['city'] == cityName].review_count.mean()
 print(avgNumOfReviews)
 
+# getting the categories that contain Restaurants
 restaurantCategory = data.loc[data['categories'].str.contains('Restaurants')]
+# getting the city that was entered in the command line and getting the average review_count
 avgNumOfReviewsBus = restaurantCategory.loc[data['city'] == cityName].review_count.mean()
 print(avgNumOfReviewsBus)
