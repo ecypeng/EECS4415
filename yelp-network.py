773dc9c9777d20981d228b3a19165a70765833ed
CelinaAssal <45611903+CelinaAssal@users.noreply.github.com>
2021-05-23 14:16:18 -0400

    Update yelp-network.py

--- yelp-network.py
+++ yelp-network.py
@@ -10,8 +10,11 @@ data = pandas.read_csv(targetFile)
 
 # filter out rows without friends
 data = data[~data['friends'].str.contains('None')]
+# splitting by , and stacking
 friendPairing = pandas.DataFrame(data.friends.str.split(',').tolist(), index = data.user_id).stack()
+# reset the index
 friendPairing = friendPairing.reset_index([0, 'user_id'])
+# setting the column names
 friendPairing.columns = ['friend1ID', 'friend2ID']
 
 # checking for duplicates + reverse duplicates
@@ -20,18 +23,18 @@ noDuplicates = pandas.DataFrame(numpy.sort(friendPairing.values, axis = 1), colu
 with open('yelp-network.txt', 'w') as myfile:
   noDuplicates.to_csv(myfile, header= None, index = False)
 
-#read input file
+# reading the contents of yelp-network.txt
 f = open("yelp-network.txt", "r")
-#read file contents to string
+# reading the contents of yelp-network.txt and setting it to data1
 data1 = f.read()
-#replace all occurrences of the required string
+# replacing all spaces with nothing (so removing spaces) and replacing all , with spaces
 data1 = data1.replace(' ', '')
 data1 = data1.replace(',', ' ')
-#close the input file
+# close the file yelp-network.txt
 f.close()
-#open the input file in write mode
-f = open("yelp-network.txt", "wt")
-#overrite the input file with the resulting data
+# opening yelp-network.txt to write to it
+f = open("yelp-network.txt", "w")
+# writing the contents of data1 to yelp-network.txt
 f.write(data1)
-#close the file
+# close the file yelp-network.txt
 f.close()
