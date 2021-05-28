e11ac05ea795ad7600707798a6506a886af54c64
CelinaAssal <45611903+CelinaAssal@users.noreply.github.com>
2021-05-27 20:53:24 -0400

    Update umapper.py

--- umapper.py
+++ umapper.py
@@ -1,13 +1,16 @@
 import sys
 import re
+import csv
 
-targetFile = open('yelp_tip.csv', 'r')
 outputFile = open('unigrams.txt', 'w')
 
-for line in sys.stdin:
-    line = re.sub( r'\W+|\W+$', '', line )
-    words = re.split(r'\W+', line)
+with open('yelp_tip.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row[0])
+        line = re.sub(r'\W+|\W+$', '', row[0])
+        words = re.split(r'\W+', row[0])
 
-    for word in words:
-        word = word.lower()
-        outputFile.write(word + "\n")
+        for word in words:
+            word = word.lower()
+            outputFile.write(word + "\t1" + "\n")
