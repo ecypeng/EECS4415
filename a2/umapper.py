22392ab98f89ee44ba9756c541429cd640af5c30
Eric <31872664+ecypeng@users.noreply.github.com>
2021-05-30 14:41:50 -0400

    Update umapper.py

--- a2/umapper.py
+++ a2/umapper.py
@@ -5,7 +5,7 @@ outputFile = open('unigrams.txt', 'w')
 allWords = []
 with open('yelp_tip.csv', 'r') as file:
     reader = csv.reader(file)
-    for row in reader:jjjj
+    for row in reader:
         line = re.sub(r'\W+|\W+$', '', row[0])
         words = re.split(r'\W+', row[0])
         for word in words:
