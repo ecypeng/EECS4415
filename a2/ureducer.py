7690b3acfadeca98d330c2503fe30ac3df3e384e
Celina Assal <celina.assal@shopify.com>
2021-05-29 21:12:01 -0400

    fixing ureducer

--- a2/ureducer.py
+++ a2/ureducer.py
@@ -16,6 +16,42 @@ import csv
 
 # print(str(numUnigram))
 
-with open('unigrams.txt', 'r') as file:
-    for line in sorted(file):
-        print(line, end='')
+# with open('unigrams.txt', 'r') as file:
+#     for line in sorted(file):
+#         print(line, end='')
+
+# import sys
+
+# previous = None
+# sum = 0
+
+# for line in sys.stdin:
+#     key, value = line.split('\t')
+
+#     if key != previous:
+#         if previous is not None:
+#             print(str(sum) + '\t' + previous)
+#         previous = key
+#         sum = 0
+
+#     sum = sum + int(value)
+# print(str(sum) + '\t' + previous)
+
+import sys
+
+previous = None
+sum = 0
+
+file = open('unigrams.txt', 'r')
+
+for line in file:
+    key, value = line.split('\t')
+
+    if key != previous:
+        if previous is not None:
+            print(str(sum) + '\t' + previous)
+        previous = key
+        sum = 0
+
+    sum = sum + int(value)
+print(str(sum) + '\t' + previous)
