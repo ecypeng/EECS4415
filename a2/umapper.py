c4bada1aef30ac43747d4d2dbcd25dcf3f483d6e
Eric <31872664+ecypeng@users.noreply.github.com>
2021-05-30 14:39:07 -0400

    Add files via upload

--- a2/umapper.py
+++ a2/umapper.py
@@ -1,16 +1,17 @@
-import sys
 import re
 import csv
 
 outputFile = open('unigrams.txt', 'w')
-
+allWords = []
 with open('yelp_tip.csv', 'r') as file:
     reader = csv.reader(file)
-    for row in reader:
-        print(row[0])
+    for row in reader:jjjj
         line = re.sub(r'\W+|\W+$', '', row[0])
         words = re.split(r'\W+', row[0])
-
         for word in words:
-            word = word.lower()
-            outputFile.write(word + "\t1" + "\n")
+            allWords.append(word.lower())
+    
+allWords.sort()    
+for i in range(len(allWords)):
+    outputFile.write(allWords[i] + "\t1\n")
+
