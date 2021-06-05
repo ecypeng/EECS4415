3826bd2e39d85e3812cc01056dc55afd03298213
Eric Peng <eric.peng@shopify.com>
2021-06-05 11:34:47 -0400

    finished reducer, part 3

--- a2/iireducer.py
+++ a2/iireducer.py
@@ -3,15 +3,35 @@
 import sys
 
 previous = None
-sum = 0
+sum = []
+formattedString = ''
 
 for line in sys.stdin:
+    # split by category and id
     busCat, busId = line.split('\t')
+
+    # if the category is same as the previous
     if busCat != previous:
+        # and is not the first
         if previous is not None:
-            print(str(busCat) + ', ' + str(sum))
+            # this for loop iterates through each value except the last one to append commas
+            for i in range(len(sum)-1):
+                formattedString += sum[i]
+                formattedString += ", "
+                # append final value
+            formattedString += sum[-1]
+            print(str(previous) + '\t' + formattedString)
+
+        # reset value of previous and sum
         previous = busCat
-        sum = 0
-    sum = sum + int(value)
-    
-print(str(busCat) + ', ' + str(sum))
+        sum = []
+
+    sum.append(str.rstrip(busId))
+
+    formattedString = ''
+    for i in range(len(sum)-1):
+        formattedString += sum[i]
+        formattedString += ", "
+    formattedString += sum[-1]
+
+print(str(previous) + ', ' + formattedString)
